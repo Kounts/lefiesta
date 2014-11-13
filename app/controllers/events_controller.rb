c024db7714098d408825850b9276591a6e574bfb
k <the.carbonk.agency@gmail.com>
2014-11-13 22:11:31 +0100

    Add update+delete on event and some CSS

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
 def new
+	@event = Event.new
 end
 
 def index
@@ -9,13 +10,35 @@ def index
 def create
 	@event = Event.new(event_params)
 
-	@event.save
+	if @event.save
 	redirect_to @event
+else
+	render 'new'
+end
 	end
 
 	def show 
 		@event = Event.find(params[:id])
 end
+
+def edit
+	@event = Event.find(params[:id])
+	end
+
+	def update
+@event = Event.find(params[:id])
+if @event.update(event_params)
+	redirect_to @event
+else render 'edit'
+end
+	end
+	
+	def destroy
+		@event = Event.find(params[:id])
+@event.destroy
+
+redirect_to events_path
+	end
 
 private
 def event_params
